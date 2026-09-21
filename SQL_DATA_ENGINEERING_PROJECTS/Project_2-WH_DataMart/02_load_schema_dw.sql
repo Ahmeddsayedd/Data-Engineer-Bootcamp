622aef046dd70696aa7877efe7c9627cc70610fd
Ahmed Aly <ahmedd0alyy@gmail.com>
2026-09-21 02:33:31 +0200

    load schemas and create master build script

---
+++ SQL_DATA_ENGINEERING_PROJECTS/Project_2-WH_DataMart/02_load_schema_dw.sql
@@ -0,0 +1,42 @@
+-- Step 2: DW - Load data from CSV files into star schema tables (Data Warehouse)
+
+
+
+SELECT '=== Loading company_dim Table ===' AS info;
+-- FIRST TABLE:
+INSERT INTO company_dim (company_id, name)
+SELECT company_id, name
+FROM read_csv_auto('https://storage.googleapis.com/sql_de/company_dim.csv');
+
+
+
+SELECT '=== Loading skills_dim Table ===' AS info;
+-- SECOND TABLE:
+INSERT INTO skills_dim (skill_id, skills, type)
+SELECT skill_id, skills, type
+FROM read_csv_auto('https://storage.googleapis.com/sql_de/skills_dim.csv');
+
+
+
+SELECT '=== Loading job_postings_fact Table ===' AS info;
+-- THIRD TABLE:
+INSERT INTO job_postings_fact (
+    job_id, company_id, job_title_short, job_title, job_location, 
+    job_via, job_schedule_type, job_work_from_home, search_location,
+    job_posted_date, job_no_degree_mention, job_health_insurance, 
+    job_country, salary_rate, salary_year_avg, salary_hour_avg
+)
+SELECT 
+    job_id, company_id, job_title_short, job_title, job_location, 
+    job_via, job_schedule_type, job_work_from_home, search_location,
+    job_posted_date, job_no_degree_mention, job_health_insurance, 
+    job_country, salary_rate, salary_year_avg, salary_hour_avg
+FROM read_csv_auto('https://storage.googleapis.com/sql_de/job_postings_fact.csv');
+
+
+
+SELECT '=== Loading skills_job_dim Table ===' AS info;
+-- FOURTH TABLE:
+INSERT INTO skills_job_dim (skill_id, job_id)
+SELECT skill_id, job_id
+FROM read_csv_auto('https://storage.googleapis.com/sql_de/skills_job_dim.csv');
